e9c60c2244f42c8d9099f17872b21c1a7dcb6767
Yalamandarao <yalamandarao.i@gmail.com>
2018-04-01 12:19:22 +0800

    - Created based project

---
+++ Localization/Localization/LocalizationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Localization
+{
+    public partial class LocalizationPage : ContentPage
+    {
+        public LocalizationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
